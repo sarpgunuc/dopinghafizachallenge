d8c9f098e0cc5b142ab9ab73de09068b17c509fc
sarpgunuc <115190060+sarpgunuc@users.noreply.github.com>
2024-05-16 17:12:02 +0300

    Projeye başlıyorum

---
+++ dopinghafizaback/src/test/java/com/doping/dopinghafizaback/DopinghafizabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doping.dopinghafizaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopinghafizabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
